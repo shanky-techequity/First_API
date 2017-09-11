1c7d151e6a4560e6641c8c1813c93e066a20d339
Shashank Gupta <shashank.gupta@techequity.in>
2017-09-11 17:27:44 +0530

    Add token

--- server/models/user.js
+++ server/models/user.js
@@ -1,13 +1,51 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
+const validator = require('validator');
+const jwt = require('jsonwebtoken')
 
-
-var User = mongoose.model('User', {
-	text:{
+var UserSchema = new mongoose.Schema({
+email:{
 		type: String,
 		required: true,
 		minlength: 1,
-		trim: true
-	}
+		trim: true,
+		unique: true,
+		validate: {
+			// validator: (value) => {
+			// 	return validator
+			// },
+			validator: validator.isEmail,
+			message: '{VALUE} is not a valid email'
+		}
+	},
+	password: {
+		type: String,
+		require: true,
+		minlength: 6
+	},
+	tokens: [{
+		access: {
+			type: String,
+			required: true
+		},
+		token: {
+			type: String,
+			required: true
+		}
+	}]
 });
+
+UserSchema.methods.generateAuthToken = function(){
+	var user  = this;
+	var access = 'auth';
+	var token = jwt.sign({_id: user._id.toHexString(),access},'abc123').toString();
+
+	user.tokens.push({access,token});
+
+	return user.save().then(() => {
+		return token;
+	});  
+};
+
+var User = mongoose.model('User', UserSchema);
 
 module.exports = {User};
